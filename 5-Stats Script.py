3ba0e95cb1d9a35b99f3af23c1e7be4d7ee9ed6c
Xavi <xafontova@gmail.com>
2018-10-28 20:08:11 +0100

    Initial Stats Script

---
+++ 5-Stats Script.py
@@ -0,0 +1,12 @@
+import logging
+import pickle
+from Stats import StatsOps
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
+with open('data/Calculated_Ops_with_Time_line.pkl', 'rb') as f:
+    ops, range_dates = pickle.load(f)
+
+a = StatsOps(ops.df_all_dates)
+b = a.open_amount()
+
