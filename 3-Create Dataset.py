0a02631cd119e0d27a6a7dcc7f3ec0b69f2dc88c
Xavi <xafontova@gmail.com>
2018-10-28 20:06:36 +0100

    Clean Stats

--- 3-Create Dataset.py
+++ 3-Create Dataset.py
@@ -1,8 +1,7 @@
 import logging
 import pickle
-
 from FeaturesLabelsGenerator import FeaturesLabelsGenerator
-from Stats import StatsOps
+
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
@@ -12,14 +11,10 @@ with open('data/Calculated_Ops_with_Time_line.pkl', 'rb') as f:
 with open('data/Pred_Cifra_csv_in_df.pkl', 'rb') as f:
     df_ops, df_history, df_op_lines = pickle.load(f)
 
-
 Xy = FeaturesLabelsGenerator(ops, df_op_lines, timedelta_in_months=6, df_changes_history=df_history)
 
 Xy.calculate_label()
 Xy.calculate_features()
-
-a = StatsOps(ops.df_all_dates)
-b = a.open_amount()
 
 with open('data/Complete_dataset.pkl', 'wb') as f:
     pickle.dump(Xy, f)
--- Stats.py
+++ Stats.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import numpy as np
 
 class StatsOps:
 
     def __init__(self, df_all_dates: pd.DataFrame):
-        self._df = df_all_dates
+        self.df = df_all_dates
         self.STAGE_TO_LABEL = {'0 - Closed': 0,
                                '7 - Deliver & Validate': 2,
                                '2 - Identify Customer Strategic Initiatives': 1,
@@ -13,7 +14,9 @@ class StatsOps:
                                '6 - Negotiate to Win': 1}
 
     def open_amount(self):
-        df = self._df
+        #TODO: Weighted?
+
+        df = self.df
 
         mask = (df['Phase/Sales Stage'].map(self.STAGE_TO_LABEL) == 1) &\
                (df['Opportunity Category'] == 'Simple') &\
@@ -21,5 +24,14 @@ class StatsOps:
         df = df[mask]
 
         return df['Amount'].groupby([df.date_ts, df['Classification Level 1']]).sum()
+
+
+    def open_weighted_amount_by_person(self):
+        #TODO: Weighted? Use forecated percentage instead?
+
+        df = self.df
+        #df.pivot_table(values='', index='', columns='', aggfunc=np.sum)
+
+
 
 
